56e05d4b89fc8d048b14d50286fe99aa4c8a9249
Chris Ewin <cewin@unimelb.edu.au>
2026-09-20 13:52:49 +1000

    Reverted app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello COMP300221 class.  Here's a nice spinning react logo to start us off.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
