bd904846c0c2ac27688c613b9120c786f4f75989
Chris Ewin <cewin@unimelb.edu.au>
2026-09-21 11:40:16 +1000

    Added a welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello COMP30022 Class.  This is a deployment.
         </p>
         <a
           className="App-link"
